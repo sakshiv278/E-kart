8c7569e03c19af7258efb596ec9b9576e8dce9cb
Sakshee Varshney <66488392+sakshiv278@users.noreply.github.com>
2022-08-03 00:45:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const cartRoutes = require('./routes/cartRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 // const paymentRoutes = require('./routes/paymentRoutes')
 
-const mongoUrl = "mongodb+srv://vikas:vikas123@apnidukan.d2e3h.mongodb.net/apniDukan?retryWrites=true&w=majority"
+const mongoUrl = ""
 
 mongoose.connect(mongoUrl,{
 	useNewUrlParser: true,
